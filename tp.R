4286c77ef4f657ca3fcb336a4c4c22ee4ed9c0d7
Fakuu0 <125106336+Fakuu0@users.noreply.github.com>
2024-10-28 14:31:30 -0300

    Add files via upload

--- tp.R
+++ tp.R
@@ -11,5 +11,5 @@ tp_filtrado <- tp %>%
   select(-pondera, -px06_agrup, -px07_agrup, -px08_agrup, -pxb16_1_1, -pxb16_1_2,
          -pxb16_1_3, -pxb16_1_4, -pxb16_1_5, -pxb16_1_6, -pxb16_1_7, -pxb16_1_9, 
          -pxb16_2, -p004, -p005, -p006, -p007, -cond_act, -p013, -j_sexo, -j_edad,
-         -j_nivel_ed)
+         -j_nivel_ed, -nivel_ed)
 
